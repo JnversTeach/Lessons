2bf078c34e5877481e298536c2268c7d05cceacf
JnverS <julya2301@gmail.com>
2020-04-02 13:47:57 +0500

    fix all chanches

--- Notebook/app/src/main/java/com/example/notebook/MainActivity.java
+++ Notebook/app/src/main/java/com/example/notebook/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.notebook;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -11,18 +12,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private final static String FILENAME = "sample.txt"; // имя файла
-    private EditText mEditText;
-    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8;
+    private EditText notePad;
+    private Button savebtn, openbtn, copybtn, pastebtn, ctrlzbtn, ctrlybtn,lagetxtbtn, smalltxtbtn;
     private ArrayList<String> ctrlz = new ArrayList<String>();
     private ArrayList<String> ctrly = new ArrayList<String>();
     private boolean needToSave=true;
@@ -32,28 +34,28 @@ public class MainActivity extends AppCompatActivity {
 
        setContentView(R.layout.activity_main);
 
-        mEditText = (EditText) findViewById(R.id.editText);
-        btn1 = (Button) findViewById(R.id.button1);
-        btn2 = (Button) findViewById(R.id.button2);
-        btn3 = (Button) findViewById(R.id.button3);
-        btn4 = (Button) findViewById(R.id.button4);
-        btn5 = (Button) findViewById(R.id.button5);
-        btn6 = (Button) findViewById(R.id.button6);
-        btn7 = (Button) findViewById(R.id.button7);
-        btn8 = (Button) findViewById(R.id.button8);
-        btn1.setOnClickListener(clckbtn1);
-        btn2.setOnClickListener(clckbtn2);
-        btn3.setOnClickListener(clckbtn3);
-        btn4.setOnClickListener(clckbtn4);
-        btn5.setOnClickListener(clckbtn5);
-        btn6.setOnClickListener(clckbtn6);
-        btn7.setOnClickListener(clckbtn7);
-        btn8.setOnClickListener(clckbtn8);
+        notePad = (EditText) findViewById(R.id.editText);
+        savebtn = (Button) findViewById(R.id.button1);
+        openbtn = (Button) findViewById(R.id.button2);
+        copybtn = (Button) findViewById(R.id.button3);
+        pastebtn = (Button) findViewById(R.id.button4);
+        ctrlzbtn = (Button) findViewById(R.id.button5);
+        ctrlybtn = (Button) findViewById(R.id.button6);
+        lagetxtbtn = (Button) findViewById(R.id.button7);
+        smalltxtbtn = (Button) findViewById(R.id.button8);
+        savebtn.setOnClickListener(clcksavebtn);
+        openbtn.setOnClickListener(clckopenbtn);
+        copybtn.setOnClickListener(clckcopybtn);
+        pastebtn.setOnClickListener(clckpastebtn);
+        ctrlzbtn.setOnClickListener(clckctrlzbtn);
+        ctrlybtn.setOnClickListener(clckctrlybtn);
+        lagetxtbtn.setOnClickListener(clcklagetxtbtn);
+        smalltxtbtn.setOnClickListener(clcksmalltxtbtn);
 
         ctrlz.add(" ");
         ctrly.add(" ");
 
-        mEditText.addTextChangedListener(new TextWatcher() {
+        notePad.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -62,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (needToSave) {
-                    ctrlz.add(mEditText.getText().toString());
+                    ctrlz.add(notePad.getText().toString());
                 }
                 else {
                     needToSave=true;
@@ -76,100 +78,83 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private View.OnClickListener clckbtn1 = new View.OnClickListener() {
+    private View.OnClickListener clcksavebtn = new View.OnClickListener() {
         public void onClick(View v) {
             saveFile(FILENAME);
         }
     };
-    private View.OnClickListener clckbtn2 = new View.OnClickListener() {
+    private View.OnClickListener clckopenbtn = new View.OnClickListener() {
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         public void onClick(View v) {
             openFile(FILENAME);
         }
     };
-    private View.OnClickListener clckbtn3 = new View.OnClickListener() {
+    private View.OnClickListener clckcopybtn = new View.OnClickListener() {
         public void onClick(View v) {
            copy();
         }
     };
-    private View.OnClickListener clckbtn4 = new View.OnClickListener() {
+    private View.OnClickListener clckpastebtn = new View.OnClickListener() {
         public void onClick(View v) {
             paste();
             }
     };
-    private View.OnClickListener clckbtn5 = new View.OnClickListener() {
+    private View.OnClickListener clckctrlzbtn = new View.OnClickListener() {
         public void onClick(View v) {
             back();
         }
     };
-    private View.OnClickListener clckbtn6 = new View.OnClickListener() {
+    private View.OnClickListener clckctrlybtn = new View.OnClickListener() {
         public void onClick(View v) {
             ahead();
             }
     };
-    private View.OnClickListener clckbtn7 = new View.OnClickListener() {
+    private View.OnClickListener clcklagetxtbtn = new View.OnClickListener() {
         public void onClick(View v) {
-            mEditText.setTextSize(TypedValue.COMPLEX_UNIT_SP ,mEditText.getTextSize()*1f);
+            notePad.setTextSize(TypedValue.COMPLEX_UNIT_SP ,notePad.getTextSize()*1f);
 
         }
     };
-    private View.OnClickListener clckbtn8 = new View.OnClickListener() {
+    private View.OnClickListener clcksmalltxtbtn = new View.OnClickListener() {
         public void onClick(View v) {
-            mEditText.setTextSize(TypedValue.COMPLEX_UNIT_SP , mEditText.getTextSize()*0.1f);
+            notePad.setTextSize(TypedValue.COMPLEX_UNIT_SP , notePad.getTextSize()*0.1f);
         }
     };
 
     // Метод для открытия файла
-    private void openFile(String fileName) {
-        FileInputStream fin = null;
-        try {
-            fin = openFileInput(fileName);
-            byte[] bytes = new byte[fin.available()];
-            fin.read(bytes);
-            String text = new String(bytes);
-            mEditText.setText(text);
-
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void openFile(String fileName) { //open text file
+        String openf = "";
+        try
+        {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    openFileInput(fileName)));
+            // читаем содержимое
+            while ((openf = reader.readLine()) != null) {
+                notePad.setText(openf);
+            }
         } catch (IOException ex) {
         Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
         }
-        finally{
-
-        try{
-            if(fin!=null)
-                fin.close();
-        }
-        catch(IOException ex){
-
-            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
-        }
     }
-    }
+
     // Метод для сохранения файла
     private void saveFile(String fileName) {
-        FileOutputStream fos = null;
+        String text = notePad.getText().toString();
         try {
-            String text = mEditText.getText().toString();
-
-            fos = openFileOutput(fileName, MODE_PRIVATE);
-            fos.write(text.getBytes());
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+                    openFileOutput(fileName, MODE_PRIVATE)));
+            writer.write(text);
+            writer.close();
             Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show();
         } catch (IOException ex) {
             Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
         }
-        finally{
-            try{
-                if(fos!=null)
-                    fos.close();
-            }
-            catch(IOException ex){
-
-                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
-            }
     }
-}
     // Метод для копирования
     private void copy() {
         ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        ClipData clip = ClipData.newPlainText("", mEditText.getText().toString());
+        ClipData clip = ClipData.newPlainText("", notePad.getText().toString());
         clipboard.setPrimaryClip(clip);
     }
     // Метод для вставки
@@ -177,22 +162,26 @@ public class MainActivity extends AppCompatActivity {
         ClipboardManager cb = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         if (cb.hasPrimaryClip()) {
             ClipData cd = cb.getPrimaryClip();
-            mEditText.setText(cd.getItemAt(0).getText().toString());
+            int index = notePad.getSelectionStart() >= 0 ? notePad.getSelectionStart() : 0;
+            StringBuilder sBuilder = new StringBuilder(cd.getItemAt(0).getText().toString()); sBuilder.insert(index, cd.getItemAt(0).getText().toString());
+            notePad.setText(sBuilder.toString());
+            notePad.setSelection(index + cd.getItemAt(0).getText().toString().length());
         }
     }
+
     // Метод отмена
     private void back() {
         needToSave = false;
         if (ctrlz.size() > 0) {
-            ctrly.add(mEditText.getText().toString());
-            mEditText.setText(ctrlz.get(ctrlz.size() - 1));
+            ctrly.add(notePad.getText().toString());
+            notePad.setText(ctrlz.get(ctrlz.size() - 1));
             ctrlz.remove(ctrlz.size() - 1);
         }
     }
     // Метод шаг вперед
    private void ahead() {
        if (ctrly.size() > 0) {
-           mEditText.setText(ctrly.get(ctrly.size() - 1));
+           notePad.setText(ctrly.get(ctrly.size() - 1));
            ctrly.remove(ctrly.size() - 1);
            needToSave = true;
        }
